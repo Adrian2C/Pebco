cec7a8265539a0debfb5aff22b0b207696d8f9fd
Adrian <adriancoceres.93@gmail.com>
2024-03-26 08:50:28 -0300

    add translations

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 // Function to fetch language data
 async function fetchLanguageData(lang) {
-  const response = await fetch(`languages/${lang}.json`);
+  const response = await fetch(`../languages/${lang}.json`);
   return response.json();
 }
 
@@ -10,7 +10,7 @@ function setLanguagePreference(lang) {
   location.reload();
 }
 
-// Function to update content based on selected language
+// Function to update content based on selected language -
 function updateContent(langData) {
   document.querySelectorAll("[data-i18n]").forEach((element) => {
     const key = element.getAttribute("data-i18n");
